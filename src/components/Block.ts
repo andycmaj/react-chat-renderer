5fa32ce465c6b186ecfc4feb47e1722b9cf765ec
Andy <andycunn@gmail.com>
2020-08-25 16:39:45 -0700

    add header block

--- src/components/Block.ts
+++ src/components/Block.ts
@@ -1,8 +1,12 @@
 import { FC } from '..';
 import { KnownBlock } from '@slack/types';
+import { HeaderBlockSpec } from './HeaderBlock';
 
 export interface BlockProps {
   blockId?: string;
 }
 
-export type Block<P extends BlockProps, B extends KnownBlock> = FC<P, B>;
+// slack types don't yet include HeaderBlock
+export type AllowedBlocks = KnownBlock | HeaderBlockSpec;
+
+export type Block<P extends BlockProps, B extends AllowedBlocks> = FC<P, B>;
