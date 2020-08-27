ea9df93f833bcf69b37925a1310a7878c2921869
Andy <andycunn@gmail.com>
2020-08-26 19:04:04 -0700

    add hint to InputBlock

--- src/components/InputBlock.ts
+++ src/components/InputBlock.ts
@@ -4,15 +4,18 @@ import { ContainerProps } from './ContainerProps';
 
 export interface InputBlockProps extends BlockProps, ContainerProps<any> {
   label: string;
+  hint: string;
 }
 
 export const InputBlock: Block<InputBlockProps, InputBlockSpec> = ({
   label,
+  hint,
   blockId,
   children,
 }) => ({
   type: 'input',
   label: { type: 'plain_text', text: label, emoji: true },
+  hint: { type: 'plain_text', text: hint, emoji: true },
   block_id: blockId,
   element: children[0],
 });
