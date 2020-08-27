ee658eb3918bc30a7ceca93e80718d0ef8aeafca
Andy <andycunn@gmail.com>
2020-08-26 19:10:50 -0700

    guard against null hint in inputBlock

--- src/components/InputBlock.ts
+++ src/components/InputBlock.ts
@@ -4,7 +4,7 @@ import { ContainerProps } from './ContainerProps';
 
 export interface InputBlockProps extends BlockProps, ContainerProps<any> {
   label: string;
-  hint: string;
+  hint?: string;
 }
 
 export const InputBlock: Block<InputBlockProps, InputBlockSpec> = ({
@@ -12,10 +12,17 @@ export const InputBlock: Block<InputBlockProps, InputBlockSpec> = ({
   hint,
   blockId,
   children,
-}) => ({
-  type: 'input',
-  label: { type: 'plain_text', text: label, emoji: true },
-  hint: { type: 'plain_text', text: hint, emoji: true },
-  block_id: blockId,
-  element: children[0],
-});
+}) => {
+  const spec: InputBlockSpec = {
+    type: 'input',
+    label: { type: 'plain_text', text: label, emoji: true },
+    block_id: blockId,
+    element: children[0],
+  };
+
+  if (hint) {
+    spec.hint = { type: 'plain_text', text: hint, emoji: true };
+  }
+
+  return spec;
+};
