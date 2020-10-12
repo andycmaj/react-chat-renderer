4a8190ddf8d3aaa8af2fa67fc4453edb9d819798
davemeyer <david.w.meyer21@gmail.com>
2020-10-12 16:05:39 -0700

    add RadioButton component (#51)
    * add RadioButton component
    
    * single initial option for radio buttons

---
+++ src/components/RadioButtons.ts
@@ -0,0 +1,20 @@
+import { RadioButtons } from '@slack/types';
+import { FC } from '..';
+import { buildInputOptions, InputOption } from './shared/inputOption';
+
+export interface RadioButtonsElementProps {
+  initialOption?: InputOption;
+  actionId: string;
+  confirm?: boolean;
+  options: InputOption[];
+}
+
+export const RadioButtonsElement: FC<
+  RadioButtonsElementProps,
+  RadioButtons
+> = ({ initialOption, actionId, options }) => ({
+  type: 'radio_buttons',
+  action_id: actionId,
+  options: buildInputOptions(options),
+  initial_option: buildInputOptions([initialOption])[0],
+});
