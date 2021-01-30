b37724e6713b9039a30b4e9d4ba57b693040f737
Andy <andycunn@gmail.com>
2021-01-29 17:07:40 -0800

    fix empty optiongroups

--- src/components/shared/inputOption.ts
+++ src/components/shared/inputOption.ts
@@ -9,25 +9,27 @@ export interface InputOption {
 }
 
 export const buildInputOptions = (inputOptions: InputOption[]): Option[] =>
-  inputOptions?.map(inputOption => {
-    if (!inputOption) {
-      return;
-    }
-    const option: Option = {
-      text: inputOption.text,
-    };
-    if (inputOption.url) {
-      option.url = inputOption.url;
-    }
-    if (inputOption.value) {
-      option.value = inputOption.value;
-    }
-    if (inputOption.description) {
-      option.description = {
-        text: inputOption.description,
-        type: 'plain_text',
-        emoji: true,
-      };
-    }
-    return option;
-  }) ?? [];
+  inputOptions
+    ? inputOptions.map(inputOption => {
+        if (!inputOption) {
+          return;
+        }
+        const option: Option = {
+          text: inputOption.text,
+        };
+        if (inputOption.url) {
+          option.url = inputOption.url;
+        }
+        if (inputOption.value) {
+          option.value = inputOption.value;
+        }
+        if (inputOption.description) {
+          option.description = {
+            text: inputOption.description,
+            type: 'plain_text',
+            emoji: true,
+          };
+        }
+        return option;
+      })
+    : [];
